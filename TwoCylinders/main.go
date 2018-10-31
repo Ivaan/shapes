922fea1e37d7344b0805b9af361b29c45fd43add
Ivaan <ivaan@nazaroff.ca>
2018-10-30 22:26:56 -0700

    Playing with a poor Grow algo Interestingly, concave vs convex behave differently and compound curves behave not at all?

--- TwoCylinders/main.go
+++ TwoCylinders/main.go
@@ -7,12 +7,40 @@ import (
 )
 
 func main() {
-	c := sdf.Cylinder3D(20, 10, 2)
+	c := sdf.Cylinder3D(20, 10, 0)
 	t := sdf.Translate3d((sdf.V3{X: 10, Y: 0, Z: 10}))
 	negC := sdf.Transform3D(c, t)
 
 	model := sdf.Difference3D(c, negC)
 
-	sdf.RenderSTL(model, 800, "twoCylinders.stl")
+	sdf.RenderSTL(NewGrowSDF3(model, -3.0), 300, "twoCylinders.stl")
+	//sdf.RenderSTL(c, 300, "twoCylinders.stl")
 
+}
+
+//GrowSDF3 is an experiment on single distance functions (a learning experience)
+type GrowSDF3 struct {
+	sdf    sdf.SDF3
+	growBy float64
+	bb     sdf.Box3
+}
+
+//GrowSDF3 creates a new GrowSDF3
+func NewGrowSDF3(sdfIn sdf.SDF3, growBy float64) sdf.SDF3 {
+	s := GrowSDF3{}
+	s.sdf = sdfIn
+	s.growBy = growBy
+	bb := sdfIn.BoundingBox()
+	s.bb = sdf.Box3{bb.Min.AddScalar(-growBy), bb.Max.AddScalar(growBy)}
+	return &s
+}
+
+//Evaluate implements sdf
+func (s *GrowSDF3) Evaluate(p sdf.V3) float64 {
+	return s.sdf.Evaluate(p) - s.growBy
+}
+
+//BoundingBox implements sdf
+func (s *GrowSDF3) BoundingBox() sdf.Box3 {
+	return s.bb
 }
