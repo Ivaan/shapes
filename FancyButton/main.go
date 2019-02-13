57ad95e1c97c867491b9862d70769b3a8bb560b3
Ivaan Nazaroff <inazaroff@gmail.com>
2019-02-13 10:45:49 -0800

    Implemented sdf version of fancy button in go.

--- FancyButton/main.go
+++ FancyButton/main.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -9,35 +8,39 @@ import (
 
 func main() {
 
-	NumberOfLoops :=  7 //Number Of Loops"
-	TotalHeight :=  1 //"Total Height"
-	DiskDiameter :=  3 //"Diameter of 3D printed disk"
-	Thickness :=  2 //Thickness of loop ring
-	p := sdf.NewPolygon()
-    c := sdf.Cylinder3D
-	cutOffHeight := params.TotalHeight / 2;
-    // var vcyl = cylinderFromZToZ;
+	NumberOfLoops := 7   //Number Of Loops"
+	TotalHeight := 12.0  //"Total Height"
+	DiskDiameter := 30.0 //"Diameter of 3D printed disk"
+	Thickness := 3.0     //Thickness of loop ring
+	cutOffHeight := TotalHeight / 2
+	// var vcyl = cylinderFromZToZ;
 
-    disk := makeFancyDisk(params.Thickness, cutOffHeight, params.DiskDiameter, params.NumberOfLoops);
+	disk := sdf.Difference3D(
+		makeFancyDisk(Thickness, cutOffHeight, DiskDiameter, NumberOfLoops),
+		sdf.MultiCylinder3D(TotalHeight*4, 1, sdf.V2Set([]sdf.V2{sdf.V2{3, 0}, sdf.V2{-3, 0}})))
 
-    // return disk
-    //     .subtract(vcyl(-cutOffHeight, cutOffHeight, 1, 6).translate([3,0,0]))
-    //     .subtract(vcyl(-cutOffHeight, cutOffHeight, 1, 6).translate([-3,0,0]))
-    //     .translate([0,0,cutOffHeight]);
+	sdf.RenderSTL(disk, 300, "tor.stl")
+	// return disk
+	//     .subtract(vcyl(-cutOffHeight, cutOffHeight, 1, 6).translate([3,0,0]))
+	//     .subtract(vcyl(-cutOffHeight, cutOffHeight, 1, 6).translate([-3,0,0]))
+	//     .translate([0,0,cutOffHeight]);
 }
 
-func cylinderFromZToZ(fromZ, toZ, radius float64, round float64) Cylinder3D {
+func cylinderFromZToZ(fromZ, toZ, radius float64, round float64) sdf.SDF3 {
 	return sdf.Transform3D(
-        sdf.Cylinder3D(toZ - fromZ, radius, round),
-        sdf.Translate3d(sdf.V3{X: 0, Y: 0, Z: fromZ}))
+		sdf.Cylinder3D(toZ-fromZ, radius, round),
+		sdf.Translate3d(sdf.V3{X: 0, Y: 0, Z: fromZ}))
 }
 
-func repeateAround(solid sdf.SDF3, times int){
-    // var copies = [];
-    // for(var i = 0; i < times; ++i){
-    //     copies.push(solid.rotateZ(i * 360 / times));
-    // }
-    // return copies;
+func repeateAround(solid sdf.SDF3, times int) []sdf.SDF3 {
+	var copies = make([]sdf.SDF3, times)
+	for i := 0; i < times; i++ {
+		copies[i] = sdf.Transform3D(
+			solid,
+			sdf.RotateZ(float64(i)*sdf.TAU/float64(times)))
+	}
+
+	return copies
 }
 
 // function makeFancyDisk(thickness, cutOffHeight, diskDiameter, numberOfLoops)
@@ -46,3 +49,19 @@ func repeateAround(solid sdf.SDF3, times int){
 //     var tor = torus({ri:thickness, ro: diskDiameter / 4, fni:12}).rotateX(angle).translate([diskDiameter/4, 0, 0]);
 //     return union(repeateAround(tor, numberOfLoops));
 // }
+
+func makeFancyDisk(thickness, cutOffHeight, diskDiameter float64, numberOfLoops int) sdf.SDF3 {
+	angle := math.Atan((cutOffHeight - (thickness / 2)) / (diskDiameter / 2))
+	tor := sdf.Transform3D(
+		torus(thickness, diskDiameter/4),
+		sdf.RotateX(angle).Mul(sdf.Translate3d(sdf.V3{X: diskDiameter / 4, Y: 0, Z: 0})))
+	return sdf.Union3D(repeateAround(tor, numberOfLoops)...)
+}
+
+func torus(r1, r2 float64) sdf.SDF3 {
+	return sdf.Revolve3D(
+		sdf.Transform2D(
+			sdf.Circle2D(r1),
+			sdf.Translate2d(sdf.V2{X: r2, Y: 0})))
+
+}
