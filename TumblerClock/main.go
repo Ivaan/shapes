8c5586371521d2331b780170a13383e90ca3e76d
Ivaan <ivaan@nazaroff.ca>
2020-11-10 19:25:41 -0800

    change to bearingless
    direct on shaft bearing with plastic tumbler on plastic ring support

--- TumblerClock/main.go
+++ TumblerClock/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 
 	"github.com/deadsy/sdfx/sdf"
@@ -9,29 +10,52 @@ import (
 //-----------------------------------------------------------------------------
 
 func main() {
-	tumblerFaceEdgeLength := 50.0
+	useBearings := false //false means direct on shaft
+	tumblerFaceEdgeLength := 26.0
 	tumblerRadius := tumblerFaceEdgeLength / 2 / math.Cos(sdf.Tau/12)
 	tumblerCornerRound := tumblerRadius * 0.05
+	tumblerSpacing := (tumblerRadius - (tumblerFaceEdgeLength / 2)) * 2
+	tumblerShortRadius := math.Sqrt(tumblerRadius*tumblerRadius - (tumblerFaceEdgeLength/2)*(tumblerFaceEdgeLength/2))
+	tumblerMinimumWallThickness := 2.0
 
 	bearingOD := 22.0
 	bearingThickness := 7.0
 	bearingHolderStopConstriction := 1.0 //horizontle and vertical chamfer distance
 	bearingHolderTolerance := 0.1
 
-	pusherNibSize := 3.0
-	pusherLength := 4.0
-	pusherTollerance := 2.2
+	shaftOD := 6.35
+	tumblerShaftTollerance := 0.4
+	tumblerShaftBearingSurfaceLength := 5.0 //length of shaft touched by tumbler and either end
+	spacerThickness := 5.0
+	spacerChamfer := 1.0
+	spacerShaftTollerance := 0.3
+	spacerTumblerTollerance := 0.5
+	spacerEdgeInFromTumblerEdge := 1.0
+
+	pusherNibSize := 2.0
+	pusherLength := 2.2
+	pusherTollerance := 1.5
 
 	tumblerOutside := makeTumblerOutside(tumblerRadius, tumblerCornerRound, tumblerFaceEdgeLength)
-	bearingHole := makeBearingHole(bearingOD, bearingThickness, bearingHolderStopConstriction, bearingHolderTolerance, tumblerFaceEdgeLength)
 
-	tracks, nibs := makePusherTracksAndNibs(pusherNibSize, pusherLength, tumblerRadius, bearingOD/2, tumblerFaceEdgeLength, pusherTollerance)
+	var insideHole sdf.SDF3
+	if useBearings {
+		insideHole = makeBearingHole(bearingOD, bearingThickness, bearingHolderStopConstriction, bearingHolderTolerance, tumblerFaceEdgeLength)
+	} else {
+		insideHole = makeShaftAndSpacerHole(shaftOD, tumblerShaftTollerance, tumblerShaftBearingSurfaceLength, spacerThickness, spacerShaftTollerance, spacerEdgeInFromTumblerEdge, spacerTumblerTollerance, tumblerFaceEdgeLength, tumblerShortRadius, tumblerSpacing, tumblerMinimumWallThickness)
+	}
 
-	holes := sdf.Union3D(bearingHole, tracks)
+	tracks, nibs := makePusherTracksAndNibs(pusherNibSize, pusherLength, tumblerRadius, tumblerShortRadius-spacerEdgeInFromTumblerEdge, tumblerFaceEdgeLength, tumblerSpacing, pusherTollerance)
+
+	holes := sdf.Union3D(insideHole, tracks)
 	tumbler := sdf.Difference3D(tumblerOutside, holes)
 	tumbler = sdf.Union3D(tumbler, nibs)
+	spacerDisk := makeSpacerDisk(shaftOD, spacerThickness, spacerChamfer, spacerShaftTollerance, spacerEdgeInFromTumblerEdge, spacerTumblerTollerance, tumblerShortRadius)
 
-	sdf.RenderSTL(tumbler, 200, "tumbler.stl")
+	sdf.RenderSTLSlow(tumbler, 400, "tumbler.stl")
+	sdf.RenderSTLSlow(spacerDisk, 400, "spacerDisk.stl")
+	// sdf.RenderSTL(tumbler, 200, "tumbler.stl")
+	// sdf.RenderSTL(spacerDisk, 200, "spacerDisk.stl")
 
 }
 
@@ -44,26 +68,27 @@ func makeTumblerOutside(tumblerRadius, tumblerCornerRound, tumblerFaceEdgeLength
 	return tumblerOutside
 }
 
-func pusherNibProfile(size float64) sdf.SDF2 {
+func pusherNibProfile(width, height float64) sdf.SDF2 {
 	return sdf.Polygon2D([]sdf.V2{
-		{-size / 2, 0},
-		{-size / 2, size},
-		{0, size * 1.5},
-		{size / 2, size},
-		{size / 2, 0},
-		{-size / 2, 0},
+		{-width / 2, 0},
+		{-width / 2, height},
+		{0, height + width*0.5},
+		{width / 2, height},
+		{width / 2, 0},
+		{-width / 2, 0},
 	})
 }
 
-func makePusherTracksAndNibs(pusherNibSize, pusherLength, tumblerRadius, innerClearance, tumblerFaceEdgeLength, pusherTollerance float64) (sdf.SDF3, sdf.SDF3) {
-	pusher2D := pusherNibProfile(pusherNibSize)
-	track2D := pusherNibProfile(pusherNibSize + pusherTollerance/2)
+func makePusherTracksAndNibs(pusherNibSize, pusherLength, tumblerRadius, innerClearance, tumblerFaceEdgeLength, tumblerSpacing, pusherTollerance float64) (sdf.SDF3, sdf.SDF3) {
+	pusher2D := pusherNibProfile(pusherNibSize, pusherNibSize+tumblerSpacing)
+	track2D := pusherNibProfile(pusherNibSize+pusherTollerance/2, pusherNibSize+tumblerSpacing+pusherTollerance)
 
 	pushers := make([]sdf.SDF3, 3)
 	tracks := make([]sdf.SDF3, 3)
 
 	for n := 0; n < 3; n++ {
-		distanceFromCenter := (tumblerRadius+innerClearance)/2 + pusherNibSize*(float64(n-1)) // three tracks one halfway between tumbler radioius and the inner clearance, and one on each side
+		//distanceFromCenter := (tumblerRadius+innerClearance)/2 + (pusherNibSize-(pusherTollerance/2))*(float64(n-1)) // three tracks one halfway between tumbler radioius and the inner clearance, and one on each side
+		distanceFromCenter := innerClearance + pusherTollerance + pusherNibSize*(float64(n)) // three tracks one halfway between tumbler radioius and the inner clearance, and one on each side
 		startAngle := float64(n) * sdf.Tau / 3
 
 		pusherArcAngle := pusherLength / distanceFromCenter
@@ -127,4 +152,38 @@ func makeBearingHole(bearingOD, bearingThickness, bearingHolderStopConstriction,
 
 	bearingHole := sdf.Union3D(topBearingHolder, topChamfer3d, constricted, bottomChamfer3d, bottomBearingHolder)
 	return bearingHole
+}
+
+func makeShaftAndSpacerHole(shaftOD, tumblerShaftTollerance, tumblerShaftBearingSurfaceLength, spacerThickness, spacerShaftTollerance, spacerEdgeInFromTumblerEdge, spacerTumblerTollerance, tumblerFaceEdgeLength, tumblerShortRadius, tumblerSpacing, tumblerMinimumWallThickness float64) sdf.SDF3 {
+	innerChamfer := (tumblerShortRadius - tumblerMinimumWallThickness) - (shaftOD/2 + tumblerShaftTollerance)
+	//innerChamfer := 10.0
+	fmt.Printf("shaftOD: %v, tumblerShaftTollerance: %v, tumblerShaftBearingSurfaceLength: %v, spacerThickness: %v, spacerShaftTollerance: %v, spacerEdgeInFromTumblerEdge: %v, spacerTumblerTollerance: %v, tumblerFaceEdgeLength: %v, tumblerShortRadius: %v, tumblerSpacing: %v, tumblerMinimumWallThickness: %v, innerChamfer: %v", shaftOD, tumblerShaftTollerance, tumblerShaftBearingSurfaceLength, spacerThickness, spacerShaftTollerance, spacerEdgeInFromTumblerEdge, spacerTumblerTollerance, tumblerFaceEdgeLength, tumblerShortRadius, tumblerSpacing, tumblerMinimumWallThickness, innerChamfer)
+
+	return sdf.Revolve3D(
+		sdf.Polygon2D([]sdf.V2{
+			{0, tumblerFaceEdgeLength / 2},
+			{tumblerShortRadius - spacerEdgeInFromTumblerEdge, tumblerFaceEdgeLength / 2},
+			{tumblerShortRadius - spacerEdgeInFromTumblerEdge, tumblerFaceEdgeLength/2 - spacerThickness - spacerTumblerTollerance},
+			{shaftOD/2 + tumblerShaftTollerance, tumblerFaceEdgeLength/2 - spacerThickness - spacerTumblerTollerance},
+			{shaftOD/2 + tumblerShaftTollerance, tumblerFaceEdgeLength/2 - spacerThickness - spacerTumblerTollerance - tumblerShaftBearingSurfaceLength},
+			{shaftOD/2 + tumblerShaftTollerance + innerChamfer, tumblerFaceEdgeLength/2 - spacerThickness - spacerTumblerTollerance - tumblerShaftBearingSurfaceLength - innerChamfer},
+			{shaftOD/2 + tumblerShaftTollerance + innerChamfer, -tumblerFaceEdgeLength/2 + tumblerShaftBearingSurfaceLength + innerChamfer},
+			{shaftOD/2 + tumblerShaftTollerance, -tumblerFaceEdgeLength/2 + tumblerShaftBearingSurfaceLength},
+			{shaftOD/2 + tumblerShaftTollerance, -tumblerFaceEdgeLength / 2},
+			{0, -tumblerFaceEdgeLength / 2},
+			{0, tumblerFaceEdgeLength / 2},
+		}),
+	)
+}
+func makeSpacerDisk(shaftOD, spacerThickness, spacerChamfer, spacerShaftTollerance, spacerEdgeInFromTumblerEdge, spacerTumblerTollerance, tumblerShortRadius float64) sdf.SDF3 {
+	return sdf.Difference3D(
+		sdf.Cylinder3D(spacerThickness, tumblerShortRadius-spacerEdgeInFromTumblerEdge-spacerTumblerTollerance, 0),
+		sdf.Union3D(
+			sdf.Cylinder3D(spacerThickness, shaftOD/2+spacerShaftTollerance, 0),
+			sdf.Transform3D(
+				sdf.Cone3D(spacerChamfer, shaftOD/2+spacerShaftTollerance, shaftOD/2+spacerShaftTollerance+spacerChamfer, 0),
+				sdf.Translate3d(sdf.V3{0, 0, (spacerThickness - spacerChamfer) / 2}),
+			),
+		),
+	)
 }
