beff543f1283d71f5b9badc247da2405e8b6b3fb
Ivaan <ivaan@nazaroff.ca>
2023-09-26 20:40:28 -0700

    adding screw voids into bottom of tray - wip

--- WaterFowlCase/main.go
+++ WaterFowlCase/main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/deadsy/sdfx/render"
 	"github.com/deadsy/sdfx/sdf"
 	v2 "github.com/deadsy/sdfx/vec/v2"
-	//v3 "github.com/deadsy/sdfx/vec/v3"
+	v3 "github.com/deadsy/sdfx/vec/v3"
 )
 
 type circ struct {
@@ -32,12 +32,68 @@ func main() {
 		{Loc: v2.Vec{X: 136.5307836180513, Y: 1.627100563552649}, R: 6},                               //1
 		{Loc: v2.Vec{X: 86.14330244975497, Y: -72.6729449002023}, R: 68.3000454637575, Concave: true}, //0
 	}
+	screwHoleRadius := 2.2
+	screwLocations := []v2.Vec{
+		{X: 102.1072999123607, Y: 80.15479331598767},
+		{X: 80.56007930185542, Y: -0.9709640961869095},
+		{X: 80.14283201431901, Y: 57.62620932295196},
+		{X: 101.3815303433159, Y: 40.36481970108936},
+		{X: 28.28427124746186, Y: -27.45584412271571},
+		{X: 14.95467069791763, Y: -15.78309782266383},
+		{X: 42.97660077726053, Y: 13.41342961679601},
+		{X: 45.3166570024517, Y: 82.46787066561872},
+		{X: 138.1307699285115, Y: 71.59320822977331},
+		{X: 137.9449971804252, Y: 0.2128870011787197},
+		{X: 103.3410329784794, Y: -0.372899436447425},
+	}
+	//footSize := v3.Vec{X: 42, Y: 5.9, Z: 2.5}
+	footRadius := 1.5
+	footLength := 39.0 //distance between circle centers
+	footLocations := [][2]v2.Vec{
+		//X: 15, Y: 75+15/2 - footLength},{Y:1}
+	}
 
-	c := circ{Loc: v2.Vec{X: 0, Y: 0}, R: 5}
-	c2D := sdf.Cut2D(circAt(c), v2.Vec{X: 5}, v2.Vec{X: -5, Y: 2})
-	c3D := sdf.Extrude3D(c2D, 1)
-	render.ToSTL(c3D, "cutTest.stl", render.NewMarchingCubesOctree(300))
+	bottom2D, err := makeProfile(circs, 0.0)
+	if err != nil {
+		panic(err)
+	}
 
+	bottomOutling2D, err := makeProfile(circs, 5)
+	if err != nil {
+		panic(err)
+	}
+
+	bottom3D := sdf.Extrude3D(bottom2D, 5)
+	bottomOutline3D := sdf.Extrude3D(bottomOutling2D, 10)
+	plate := sdf.Difference3D(
+		bottomOutline3D,
+		sdf.Transform3D(
+			bottom3D,
+			sdf.Translate3d(v3.Vec{Z: 2.5}),
+		),
+	)
+
+	screwHoles := make([]sdf.SDF3, len(screwLocations))
+	for i, sl2D := range screwLocations {
+		sl3D := v3.Vec{X: sl2D.X, Y: sl2D.Y, Z: 0}
+		sh, err := sphereAt(sl3D, screwHoleRadius)
+		if err != nil {
+			panic(err)
+		}
+		screwHoles[i] = sh
+	}
+	plate = sdf.Difference3D(plate, sdf.Union3D(screwHoles...))
+	render.ToSTL(plate, "bottom.stl", render.NewMarchingCubesUniform(500))
+}
+
+func makeProfile(circs []circ, expand float64) (sdf.SDF2, error) {
+	for i := range circs {
+		if circs[i].Concave {
+			circs[i].R -= expand
+		} else {
+			circs[i].R += expand
+		}
+	}
 	circs2Ds := make([]sdf.SDF2, 0, len(circs))
 	circs2DHoles := make([]sdf.SDF2, 0)
 	polyPoints := make([]v2.Vec, len(circs)*2)
@@ -66,40 +122,10 @@ func main() {
 	}
 	p, err := sdf.Polygon2D(polyPoints)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	fmt.Println("len(circs2Ds)", len(circs2Ds))
-	fmt.Println("len(circs2DHoles)", len(circs2DHoles))
 	bottom2D := sdf.Union2D(sdf.Union2D(circs2Ds...), p)
-	bottom2D = sdf.Difference2D(bottom2D, sdf.Union2D(circs2DHoles...))
-	bottom3D := sdf.Extrude3D(bottom2D, 5)
-
-	render.ToSTL(bottom3D, "bottom.stl", render.NewMarchingCubesUniform(3000))
-	// c1 := circ{Loc: v2.Vec{X: 10, Y: 0}, R: 4}
-	// c2 := circ{Loc: v2.Vec{X: 0, Y: -10}, R: 6}
-	// c3 := circ{Loc: v2.Vec{X: -10, Y: 0}, R: 4, Concave: true}
-	// c4 := circ{Loc: v2.Vec{X: 0, Y: 6}, R: 6, Concave: false}
-
-	// va, va2, err := tangentPoints(c1, c2)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// vb, vb2, err := tangentPoints(c2, c3)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// vc, vc2, err := tangentPoints(c3, c4)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// vd, vd2, err := tangentPoints(c4, c1)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// circs2D := sdf.Union2D(circAt(c1), circAt(c2), circAt(c3), circAt(c4), lineFromTo(va2, va), lineFromTo(vb2, vb), lineFromTo(vc2, vc), lineFromTo(vd2, vd))
-	// c3D := sdf.Extrude3D(circs2D, 0.1)
-	// //p := sdf.Polygon2D()
-	// render.ToSTL(c3D, "circ.stl", render.NewMarchingCubesUniform(500))
+	return sdf.Difference2D(bottom2D, sdf.Union2D(circs2DHoles...)), nil
 }
 
 func tangentPoints(c1, c2 circ) (v2.Vec, v2.Vec, error) {
@@ -177,6 +203,22 @@ func circAt(c circ) sdf.SDF2 {
 	return sdf.Transform2D(c2D, sdf.Translate2d(c.Loc))
 }
 
+func sphereAt(loc v3.Vec, r float64) (sdf.SDF3, error) {
+	s, err := sdf.Sphere3D(r)
+	if err != nil {
+		return nil, err
+	}
+	return sdf.Transform3D(s, sdf.Translate3d(loc)), nil
+}
+
+func footAt(start, direction v2.Vec, r, length float64) sdf.SDF2{
+	circs := []circ{
+		{Loc: start, R: r},
+		{Loc: start.Add(direction.Normalize().Mul(length))}
+	}
+	return makeProfile(circs, 0)
+}
+
 func lineFromTo(from, to v2.Vec) sdf.SDF2 {
 	s := to.Sub(from)
 	l := sdf.Line2D(s.Length(), 0.1)
@@ -187,6 +229,9 @@ func lineFromTo(from, to v2.Vec) sdf.SDF2 {
 	)
 }
 
+//circleTangents returns the two tangets of the given circle that intersect the given point
+//an error is returned if the point is within the circle
+//the case of the point being on the edge of the circle is also considered this error (perhaps erroneously)
 func circleTangents(c circ, p v2.Vec) (v2.Vec, v2.Vec, error) {
 	p = p.Sub(c.Loc)
 
