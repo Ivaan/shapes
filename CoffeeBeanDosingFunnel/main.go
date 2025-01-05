54f3107a22155edb0db2e7c49ad66d6c922ff6e0
Ivaan <ivaan@nazaroff.ca>
2025-01-05 13:23:44 -0800

    Angled CoffeeDosing spout
    And made it more funnel like.

--- CoffeeBeanDosingFunnel/main.go
+++ CoffeeBeanDosingFunnel/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	cupInnerWidth := 40.0 //describing the cub by its hole because this is the important bit
-	cupInnerLength := 60.0
+	cupInnerLength := 70.0
 	cupRadius := cupInnerWidth / 2.0
 	cupOtherRadius := cupInnerLength - cupRadius
 	cupThickness := 2.0
@@ -19,24 +19,20 @@ func main() {
 	spoutOuterRadius := 10.0 //describing the spout because this needs to fit in the flask/vial opening
 	spoutLength := 5.0
 	spoutThickness := 1.0
+	rotateAngle := 1.0 / 16.0 * sdf.Tau
+	rotate := sdf.RotateY(rotateAngle)
 
 	cupHole, err := ReallyWarpedSphere3D(cupRadius, cupOtherRadius)
 	if err != nil {
 		panic(err)
 	}
+	cupHole = sdf.Transform3D(cupHole, rotate)
 	cup, err := ReallyWarpedSphere3D(cupRadius+cupThickness, cupOtherRadius+cupThickness)
 	if err != nil {
 		panic(err)
 	}
-	stripe, err := sdf.Cylinder3D(stripeWidth, cupRadius+stripeThickness, stripeThickness)
-	stripe = sdf.Transform3D(
-		stripe,
-		sdf.Translate3d(v3.Vec{Z: -stripeWidth / 2}),
-	)
-	cup = sdf.Union3D(cup, stripe)
-	cup = sdf.Difference3D(cup, cupHole)
-	cup = sdf.Cut3D(cup, v3.Vec{}, v3.Vec{X: 1})
-	cup = sdf.Cut3D(cup, v3.Vec{X: cupRadius + cupThickness}, v3.Vec{X: -1})
+	cup = sdf.Transform3D(cup, rotate)
+	// cup = sdf.Cut3D(cup, v3.Vec{}, v3.Vec{X: 1})
 
 	spout, err := sdf.Cylinder3D(spoutLength, spoutOuterRadius, spoutThickness)
 	if err != nil {
@@ -46,12 +42,12 @@ func main() {
 	// x^2 = r^2 - y^2
 	// x = sqrt(r^2 - y^2)
 	// x = sqrt(cupRadius^2 - (spoutRadius-spoutThickness)^2)*cupOtherRadius/cupkradius
-
 	spoutLocation := (math.Sqrt(cupRadius*cupRadius-(spoutOuterRadius-spoutThickness)*(spoutOuterRadius-spoutThickness)) * cupOtherRadius / cupRadius)
 	spout = sdf.Transform3D(
 		spout,
 		sdf.Translate3d(v3.Vec{Z: -spoutLocation - spoutLength/2}),
 	)
+	spout = sdf.Transform3D(spout, rotate)
 	spoutHoleLength := math.Max(cupOtherRadius+cupThickness-spoutLocation, spoutLength)
 	spoutHole, err := sdf.Cylinder3D(spoutHoleLength, spoutOuterRadius-spoutThickness, 0)
 	if err != nil {
@@ -61,9 +57,49 @@ func main() {
 		spoutHole,
 		sdf.Translate3d(v3.Vec{Z: -spoutLocation - spoutHoleLength/2}),
 	)
+	spoutHole = sdf.Transform3D(spoutHole, rotate)
+	stripe, err := sdf.Cylinder3D(stripeWidth, cupRadius+stripeThickness, stripeThickness)
+	stripe = sdf.Transform3D(
+		stripe,
+		sdf.Translate3d(v3.Vec{Z: -stripeWidth / 2}),
+	)
+	cup = sdf.Union3D(cup, stripe)
+	cup = sdf.Difference3D(cup, cupHole)
+
+	cutWidth := cupInnerWidth + 2*cupThickness + 2*stripeThickness
+	cutLength := cupInnerLength + 2*cupThickness
+	cutAngle := 4.0 / 32.0 * sdf.Tau
+	cutRoundRadius := 25.0
+
+	cutCylinder, err := sdf.Cylinder3D(cutWidth, cutRoundRadius, 0)
+	if err != nil {
+		panic(err)
+	}
+	cutBox, err := sdf.Box3D(v3.Vec{X: cutLength, Y: cutWidth, Z: cutWidth}, 0)
+	if err != nil {
+		panic(err)
+	}
+	cutBox0 := sdf.Transform3D(cutBox, sdf.Translate3d(v3.Vec{X: cutLength / 2.0, Y: -cutWidth/2.0 + cutRoundRadius}))
+	cutBox1 := sdf.Transform3D(cutBox, sdf.Translate3d(v3.Vec{X: -cutLength / 2.0, Y: -cutWidth/2.0 + cutRoundRadius}))
+	cutBox1 = sdf.Transform3D(cutBox1, sdf.RotateZ(cutAngle))
+
+	cutOpening := sdf.Union3D(cutCylinder, cutBox0, cutBox1)
+	render.ToSTL(cutOpening, "cutOpening.stl", render.NewMarchingCubesUniform(300))
+	cutOpening = sdf.Transform3D(
+		cutOpening,
+		sdf.Translate3d(v3.Vec{X: -cutRoundRadius}).Mul(
+			sdf.RotateZ(-1.0/4.0*sdf.Tau).Mul(
+				sdf.RotateY(-1.0/4.0*sdf.Tau),
+			),
+		),
+	)
+	render.ToSTL(cutOpening, "cutOpeningMoved.stl", render.NewMarchingCubesUniform(300))
 
 	funnel := sdf.Union3D(cup, spout)
 	funnel = sdf.Difference3D(funnel, spoutHole)
+	funnel = sdf.Difference3D(funnel, cutOpening)
+	funnel = sdf.Cut3D(funnel, v3.Vec{X: cupRadius + cupThickness}, v3.Vec{X: -1})
+
 	render.ToSTL(funnel, "funnel.stl", render.NewMarchingCubesUniform(300))
 }
 
